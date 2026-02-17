f2a146f4fffb61c902686f1bedd29cc366f76817
oneulddu <oneulddu@users.noreply.github.com>
2026-02-18 00:48:37 +0900

    chore: add GitHub Raw auto-update metadata

---
+++ dcinside_premium_theme.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         DCinside Premium Theme (Mobile)
+// @namespace    https://github.com/oneulddu/dcinside-premium-theme
+// @version      1.2
+// @description  디시인사이드 모바일을 세련되고 모던한 프리미엄 테마로 변경합니다.
+// @author       Antigravity
+// @match        https://m.dcinside.com/*
+// @updateURL    https://raw.githubusercontent.com/oneulddu/dcinside-premium-theme/master/dcinside_premium_theme.meta.js
+// @downloadURL  https://raw.githubusercontent.com/oneulddu/dcinside-premium-theme/master/dcinside_premium_theme.user.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
